36e1063a5c4d19450bc66fb300b5a9a63b3015c1
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2019-03-05 17:29:23 -0500

    Early Character Prototype (W.I.P.), Joystick Input + TextMeshProFiles

---
+++ Spermato/Assets/Scripts/Character/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    private float m_rotationValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.E))
+            m_rotationValue++;
+
+        if (Input.GetKey(KeyCode.Q))
+            m_rotationValue--;
+
+        if ((int)transform.rotation.z != (int)m_rotationValue)
+            transform.rotation = Quaternion.Euler(0,0,m_rotationValue);
+    }
+}
---
+++ Spermato/Assets/Scripts/UI/DebugUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DebugUI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_characterRef;
+    [SerializeField] private TextMeshProUGUI m_angleValue;
+    [SerializeField] private TextMeshProUGUI m_horizAxisValue;
+    [SerializeField] private TextMeshProUGUI m_vertAxisValue;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_angleValue.text = "Angle : " + Mathf.Abs(m_characterRef.transform.rotation.z);
+        m_horizAxisValue.text = "HorizAxisValue : " + Input.GetAxis("Horizontal");
+        m_vertAxisValue.text = "VertAxisValue : " + Input.GetAxis("Vertical");
+
+
+    }
+}
