fc6f879ee21940fe8d1b01ce9b8069769dc57c5e
Valentin <v.vanloon@student.isartdigital.com>
2019-03-06 16:59:57 -0500

    Power up collision script started

---
+++ Spermato/Assets/Scripts/Character/Pick_Up_Collision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pick_Up_Collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	   	
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+        
+    }
+}
