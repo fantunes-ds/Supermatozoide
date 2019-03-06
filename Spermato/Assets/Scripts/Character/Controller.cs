2e38a8455617a4a7717e37096b9844ba11ae3703
Valentin <v.vanloon@student.isartdigital.com>
2019-03-06 14:58:36 -0500

    Basic controller added

--- Spermato/Assets/Scripts/Character/Controller.cs
+++ Spermato/Assets/Scripts/Character/Controller.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-
-    private float m_rotationValue;
+    private float m_speed = 5.0f;
+    //private float m_rotationValue;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +15,7 @@ public class Controller : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.E))
+        /*if (Input.GetKey(KeyCode.E))
             m_rotationValue++;
 
         if (Input.GetKey(KeyCode.Q))
@@ -23,5 +23,10 @@ public class Controller : MonoBehaviour
 
         if ((int)transform.rotation.z != (int)m_rotationValue)
             transform.rotation = Quaternion.Euler(0,0,m_rotationValue);
+        */
+
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        transform.Translate(Movement * Time.deltaTime * m_speed);
     }
 }
