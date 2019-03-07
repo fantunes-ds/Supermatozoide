6ef364b7130d659da8c3a5b4b389717f1bb90e94
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2019-03-07 14:49:47 -0500

    Basic character controller setup. Needs testing. Still have to comment and optimize (if possible)

--- Spermato/Assets/Scripts/Character/Controller.cs
+++ Spermato/Assets/Scripts/Character/Controller.cs
@@ -4,29 +4,57 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    private float m_speed = 5.0f;
+    public bool isLeft { get; private set; }
     [SerializeField] private int angleErrorScope;
-    //private float m_rotationValue;
+
+    [SerializeField] private float m_baseSpeed = 5.0f;
+    private float m_zRot = 0.0f;
+    private float j2Angle = 5.0f;
+
+    private Rigidbody2D m_rb;
+        float leftJoyStickHorizValue = Input.GetAxis("Horizontal");
+        float leftJoyStickVertValue = Input.GetAxis("Vertical");
+        float rightJoyStickHorizValue = Input.GetAxis("J2 Horizontal");
+        float rightJoyStickVertValue = Input.GetAxis("J2 Vertical");
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Start()
     {
-        float zRot = Mathf.Atan2(Input.GetAxis("Vertical"),
-                            Input.GetAxis("Horizontal")) * (180 / Mathf.PI) + 90;
-        transform.eulerAngles = new Vector3(0,0, zRot);
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftJoyStickHorizValue != Input.GetAxis("Horizontal") || leftJoyStickVertValue != Input.GetAxis("Vertical"))
+            m_zRot = Mathf.Atan2(Input.GetAxis("Vertical"),
+                     Input.GetAxis("Horizontal")) * (180 / Mathf.PI) + 270;
+
+        if (m_zRot != transform.rotation.eulerAngles.z)
+            transform.eulerAngles = new Vector3(0, 0, m_zRot);
+
+        j2Angle = Mathf.Atan2(Input.GetAxis("J2 Vertical"),
+                      Input.GetAxis("J2 Horizontal")) * (180 / Mathf.PI) + 270;
+
+        int detectionOffset = 90;
+
+        if (isLeft)
+            detectionOffset *= -1;
+
+        float minimumValue = m_zRot - detectionOffset - (angleErrorScope * 0.5f);
+        float maximumValue = m_zRot - detectionOffset + (angleErrorScope * 0.5f);
 
 
-        float j2Angle = Mathf.Atan2(Input.GetAxis("J2 Vertical"),
-                            Input.GetAxis("J2 Horizontal")) * (180 / Mathf.PI) + 90;
+        if ((j2Angle >= minimumValue && j2Angle <= maximumValue)
+            ||
+            (j2Angle >= minimumValue - 360 && j2Angle <= maximumValue - 360)
+            ||
+            (j2Angle >= minimumValue + 360 && j2Angle <= maximumValue + 360))
+        {
+            isLeft = !isLeft;
+            m_rb.velocity += new Vector2(transform.up.x, transform.up.y) * m_baseSpeed;
+        }
 
-        if (j2Angle >= transform.rotation.eulerAngles.z - 90 + angleErrorScope &&
-            j2Angle <= transform.rotation.eulerAngles.z - 90 - angleErrorScope)
-            Debug.Log("yes" + transform.rotation.eulerAngles.z + "  " + j2Angle);    
         //transform.position += transform.up;
     }
 }
--- Spermato/Assets/Scripts/UI/DebugUI.cs
+++ Spermato/Assets/Scripts/UI/DebugUI.cs
@@ -8,6 +8,7 @@ public class DebugUI : MonoBehaviour
     [SerializeField] private GameObject m_characterRef;
     [SerializeField] private List<TextMeshProUGUI> m_texts;
     [SerializeField] private int angleErrorScope;
+    private bool isLeft;
 
     // Use this for initialization
     void Start()
@@ -17,16 +18,23 @@ public class DebugUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_texts[0].text = "Left Joystick Angle : " + (int)(Mathf.Atan2(Input.GetAxis("Vertical"),
-                                                               Input.GetAxis("Horizontal")) * (180 / Mathf.PI) + 90);
-        m_texts[1].text = "Right Joystick Angle : " + (int)(Mathf.Atan2(Input.GetAxis("J2 Vertical"),
-                                                               Input.GetAxis("J2 Horizontal")) * (180 / Mathf.PI) + 90);
+        isLeft = m_characterRef.GetComponent<Controller>().isLeft;
+
+        int detectionOffset = 90;
+        if (isLeft)
+            detectionOffset *= -1;
+
+        int j1Input = (int) (Mathf.Atan2(Input.GetAxis("Vertical"),
+                                 Input.GetAxis("Horizontal")) * (180 / Mathf.PI) + 270);
+        int j2Input = (int) (Mathf.Atan2(Input.GetAxis("J2 Vertical"),
+                                         Input.GetAxis("J2 Horizontal")) * (180 / Mathf.PI) + 270);
+
+        m_texts[0].text = "Left Joystick Angle : " + j1Input;
+        m_texts[1].text = "Right Joystick Angle : " + j2Input;
         m_texts[2].text = "TargetAngle : Between " +
-                          (int)(m_characterRef.transform.rotation.eulerAngles.z - 180 -
-                                          angleErrorScope * 0.5f) +
+                          (j1Input - detectionOffset + (angleErrorScope * 0.5f)) +
                           " and " +
-                          (int)(m_characterRef.transform.rotation.eulerAngles.z - 180 +
-                                          angleErrorScope * 0.5f);
+                          (j1Input - detectionOffset + (angleErrorScope * 0.5f));
         m_texts[3].text = "HorizAxisValue : " + Input.GetAxis("J2 Horizontal");
         m_texts[4].text = "VertAxisValue : " + Input.GetAxis("J2 Vertical");
     }
