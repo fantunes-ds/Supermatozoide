99cddc355bd2ad74605a3daff69b8d757ae24eab
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2019-03-13 16:12:37 -0400

    Added gauge, have to see max values again + bad inputs

--- Spermato/Assets/Scripts/Character/Controller.cs
+++ Spermato/Assets/Scripts/Character/Controller.cs
@@ -6,11 +6,15 @@ public class Controller : MonoBehaviour
 {
     private bool isLeft;
 
-    [SerializeField][Tooltip("Half the amount per side. ie: 20 = 10 L + 10 R")]
+    [SerializeField] [Tooltip("Half the amount per side. ie: 20 = 10 L + 10 R")]
     private int angleErrorScope;
 
-    [SerializeField][Tooltip("Constant base speed for the player")]
-    private float m_baseSpeed = 5.0f;
+    [SerializeField] [Tooltip("Constant base speed for the player")]
+    private float m_baseSpeed = 1.0f;
+
+    [SerializeField] [Tooltip("The max amount of velocity that can be achieved. Affects the gauge")]
+    public float m_maxSpeed = 100f;
+
     private float m_progesterone = 1.0f;
     public float m_finalSpeed { get; private set; }
 
@@ -40,12 +44,15 @@ public class Controller : MonoBehaviour
 
     void SetRotation()
     {
+        if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+            return;
+
         if (leftJoyStickHorizValue != Input.GetAxis("Horizontal") || leftJoyStickVertValue != Input.GetAxis("Vertical"))
         {
             leftJoyStickHorizValue = Input.GetAxis("Horizontal");
             leftJoyStickVertValue = Input.GetAxis("Vertical");
-            m_zRot = Mathf.Atan2(leftJoyStickHorizValue,
-                         leftJoyStickVertValue) * (180 / Mathf.PI) + 270;
+            m_zRot = Mathf.Atan2(leftJoyStickVertValue,
+                         leftJoyStickHorizValue) * Mathf.Rad2Deg + 270;
         }
 
         if (m_zRot != transform.rotation.eulerAngles.z)
@@ -54,14 +61,18 @@ public class Controller : MonoBehaviour
 
     void CheckDisplacement()
     {
+        if (Input.GetAxis("J2 Vertical") == 0 && Input.GetAxis("J2 Horizontal") == 0)
+            return;
+
         int detectionOffset = 90;
 
         j2Angle = Mathf.Atan2(Input.GetAxis("J2 Vertical"),
                       Input.GetAxis("J2 Horizontal")) * (180 / Mathf.PI) + 270;
-        
+
         if (isLeft)
             detectionOffset *= -1;
 
+        m_finalSpeed = m_baseSpeed * m_progesterone;
         //m_zRot is defined in SetRotation()
         float minimumValue = m_zRot - detectionOffset - (angleErrorScope * 0.5f);
         float maximumValue = m_zRot - detectionOffset + (angleErrorScope * 0.5f);
@@ -69,13 +80,27 @@ public class Controller : MonoBehaviour
 
         if ((j2Angle >= minimumValue && j2Angle <= maximumValue)
             ||
-            (j2Angle >= minimumValue - 360 && j2Angle <= maximumValue - 360)    // - one turn
+            (j2Angle >= minimumValue - 360 && j2Angle <= maximumValue - 360) // - one turn
             ||
-            (j2Angle >= minimumValue + 360 && j2Angle <= maximumValue + 360))   // + one turn
+            (j2Angle >= minimumValue + 360 && j2Angle <= maximumValue + 360)) // + one turn
         {
-            m_finalSpeed = m_baseSpeed * m_progesterone;
-            isLeft = !isLeft;
-            m_rb.velocity += new Vector2(transform.up.x, transform.up.y) * m_finalSpeed;
+            if (m_rb.velocity.magnitude + (transform.up.magnitude * m_finalSpeed) < m_maxSpeed)
+            {
+                m_rb.velocity += new Vector2(transform.up.x, transform.up.y) * m_finalSpeed;
+                isLeft = !isLeft;
+            }
         }
+        /*else if ((j2Angle < minimumValue || j2Angle > maximumValue)
+                 ||
+                 (j2Angle < minimumValue - 360 || j2Angle > maximumValue - 360) // - one turn
+                 ||
+                 (j2Angle < minimumValue + 360 || j2Angle > maximumValue + 360)) // + one turn
+        {
+            if (m_rb.velocity.magnitude + (transform.up.magnitude * m_finalSpeed) < m_maxSpeed)
+            {
+                m_rb.velocity -= new Vector2(transform.up.x, transform.up.y) * m_finalSpeed;
+                isLeft = !isLeft;
+            }
+        }*/
     }
 }
---
+++ Spermato/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_playerRef;
+    [SerializeField] private Image m_speedGauge;
+    private float m_gaugeValue;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_gaugeValue = m_playerRef.GetComponent<Rigidbody2D>().velocity.magnitude / m_playerRef.GetComponent<Controller>().m_maxSpeed;
+        m_speedGauge.fillAmount = m_gaugeValue;
+    }
+}
