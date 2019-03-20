da09ace86d10d6b5b5eadefa2c2b2eb15a570a22
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2019-03-20 15:40:21 -0400

    Progesterone Boost added.

--- Spermato/Assets/Scripts/Character/Controller.cs
+++ Spermato/Assets/Scripts/Character/Controller.cs
@@ -14,22 +14,23 @@ public class Controller : MonoBehaviour
     [Tooltip("Constant base speed for the player")]
     private float m_baseSpeed = 1.0f;
 
-    [SerializeField]
-    [Tooltip("Ajoute du AirTime")]
-    [Range(0, 100)]
+    [SerializeField] [Tooltip("Adds AirTime")] [Range(0, 100)]
     private float m_airTimePercentage = 1f;
 
-    [SerializeField]
-    [Tooltip("How much should the progesterone boost be (0 to 1 = 0 to 100%)")]
-    [Range(0, 1)]
-    private float m_progesteroneBoost = 0.05f;
+    [SerializeField] [Tooltip("How much should the progesterone pickups add to the jauge (0 to 1 = 0 to 100%)")] [Range(0, 1)]
+    private float m_progesteronePickupValue = 0.05f;
+
+    [SerializeField] [Tooltip("How much should the progesterone boost force be (5 is a lot)")]
+    private float m_progesteroneBoostForce = 5;
+
+    [Tooltip("Maximum Progesterone that can be stored (affects the bar)")][Range(0,10)]
+    public float m_maxProgesterone = 1f;
 
     [Tooltip("The max amount of velocity that can be achieved. Affects the gauge")]
     public float m_maxSpeed;
     #endregion
 
     public float m_progesterone { get; private set; }
-    public float m_finalSpeed { get; private set; }
 
     private float m_zRot;
 
@@ -42,8 +43,6 @@ public class Controller : MonoBehaviour
     void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
-        m_progesterone = 1.0f;
-        m_finalSpeed = m_baseSpeed * m_progesterone;
     }
 
     // Update is called once per frame
@@ -51,6 +50,7 @@ public class Controller : MonoBehaviour
     {
         SetRotation();
         CheckDisplacement();
+        ProgesteroneJauge();
     }
 
     void SetRotation()
@@ -74,11 +74,13 @@ public class Controller : MonoBehaviour
     void CheckDisplacement()
     {
         m_rightJoyStickAxis = new Vector2(Mathf.RoundToInt(Input.GetAxis("J2 Horizontal")), Mathf.RoundToInt(Input.GetAxis("J2 Vertical")));
+
         if (m_rightJoyStickAxis.x.Equals(0))
         {
             m_canInput = true;
             return;
         }
+
         if (!m_canInput)
             return;
 
@@ -86,20 +88,28 @@ public class Controller : MonoBehaviour
 
         if (m_isLeft)
             axisTargetValue *= -1;
-
-        m_finalSpeed = m_baseSpeed * m_progesterone;
+        
 
         if (m_rightJoyStickAxis.x.Equals(axisTargetValue))
         {
             if (m_rb.velocity.magnitude < m_maxSpeed)
-                m_rb.AddForce(new Vector2(transform.up.x, transform.up.y) * m_finalSpeed);
+                m_rb.AddForce(new Vector2(transform.up.x, transform.up.y) * m_baseSpeed);
             m_canInput = false;
             m_isLeft = !m_isLeft;
         }
     }
 
+    public void ProgesteroneJauge()
+    {
+        if (Input.GetAxisRaw("Trigger Right").Equals(1))
+        {
+            m_rb.AddForce(transform.up * m_progesterone * m_progesteroneBoostForce,ForceMode2D.Impulse);
+            m_progesterone = 0;
+        }
+    }
+
     public void AddProgesterone()
     {
-        m_progesterone += m_progesteroneBoost;
+        m_progesterone += m_progesteronePickupValue;
     }
 }
--- Spermato/Assets/Scripts/UI/UIManager.cs
+++ Spermato/Assets/Scripts/UI/UIManager.cs
@@ -9,21 +9,28 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] private GameObject m_playerRef;
     [SerializeField] private Image m_speedGauge;
-    [SerializeField] private TextMeshProUGUI m_progesteroneText;
+    [SerializeField] private Image m_progesteroneGauge;
     private float m_gaugeValue;
+    private float m_progesteroneValue;
+    private Rigidbody2D m_playerRB;
+    private Controller m_playerController;
 
 	// Use this for initialization
 	void Start ()
     {
 		if (m_playerRef == null)
             m_playerRef = GameObject.FindGameObjectWithTag("Player");
-	}
+
+        m_playerRB = m_playerRef.GetComponent<Rigidbody2D>();
+        m_playerController = m_playerRef.GetComponent<Controller>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        m_gaugeValue = m_playerRef.GetComponent<Rigidbody2D>().velocity.magnitude / m_playerRef.GetComponent<Controller>().m_maxSpeed;
+        m_gaugeValue = m_playerRB.velocity.magnitude / m_playerController.m_maxSpeed;
         m_speedGauge.fillAmount = m_gaugeValue;
-        m_progesteroneText.text = "Progesterone Level : " + m_playerRef.GetComponent<Controller>().m_progesterone;
+        m_progesteroneValue = m_playerController.m_progesterone / m_playerController.m_maxProgesterone;
+        m_progesteroneGauge.fillAmount = m_progesteroneValue;
     }
 }
