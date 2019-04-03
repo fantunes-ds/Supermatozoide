428e21a2df8a98abea4c3ffda08fc6d5e68c5b5b
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2019-04-03 15:42:01 -0400

    Added new Camera Behaviour, to be tested

---
+++ Spermato/Assets/Scripts/Camera/CameraBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    Vector3 m_camPos = Vector3.zero;
+    public Transform m_target;
+    private Rigidbody2D m_targetRb;
+    [SerializeField] private Vector3 m_offset = new Vector3(0, 0, -1);
+    [SerializeField] private float m_travelDelay = 0.3f;
+    private float m_cameraVelocityX;
+    private float m_cameraVelocityY;
+    [SerializeField] private float m_cameraMaxVelocity = 200;
+    [SerializeField] private float m_offsetAmount = 5;
+
+    void Start()
+    {
+        if (m_target == null || m_targetRb == null)
+        {
+            try
+            {
+                m_target = GameObject.FindGameObjectWithTag("Player").transform;
+                m_targetRb = m_target.gameObject.GetComponent<Rigidbody2D>();
+                transform.position = m_target.position;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (m_target == null || m_targetRb == null)
+            return;
+
+        Vector2 m_desiredDirection = (Vector2)m_target.position + (m_targetRb.velocity * m_offsetAmount);
+        m_camPos = m_target.position;
+        m_camPos = new Vector3(Mathf.SmoothDamp(transform.position.x,m_desiredDirection.x, ref m_cameraVelocityX, m_travelDelay, m_cameraMaxVelocity, Time.fixedDeltaTime), 
+                               Mathf.SmoothDamp(transform.position.y, m_desiredDirection.y, ref m_cameraVelocityY, m_travelDelay, m_cameraMaxVelocity, Time.fixedDeltaTime));
+}
+
+    void LateUpdate()
+    {
+        transform.position = m_camPos + m_offset;
+    }
+}
